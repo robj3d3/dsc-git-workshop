1b7d419067bd207ecfa5bf5b884aa37adb923da1
Robert Hallam <roboffice100@gmail.com>
2020-11-24 18:58:44 +0000

    Add confusing functions

--- workshop.py
+++ workshop.py
@@ -1,4 +1,9 @@
+import coolfeature
 print("Hello World!")
 
 def main():
-  print("This function was added from GitHub")
+  print("This function wasn't added from GitHub")
+  print(coolfeature.confusing())
+  print(coolfeature.giraffe())
+
+main()
